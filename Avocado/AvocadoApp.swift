94f2b9f6cf34379d9afec0d5039c116b4fdd9764
Moklesur Rahman <mmrrahman508@gmail.com>
2022-01-13 23:41:38 +0600

    Initial Commit

---
+++ Avocado/AvocadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoApp.swift
+//  Avocado
+//
+//  Created by Moklesur Rahman on 13/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
