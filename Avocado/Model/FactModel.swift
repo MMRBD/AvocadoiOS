6eec4bee439d097528f311640eb6a98452bdf932
Moklesur Rahman <mmrrahman508@gmail.com>
2023-02-23 11:31:45 +0600

    silly changes

---
+++ Avocado/Model/FactModel.swift
@@ -0,0 +1,14 @@
+//
+//  FactModelView.swift
+//  Avocado
+//
+//  Created by Moklesur Rahman on 26/3/22.
+//
+
+import SwiftUI
+
+struct Fact: Identifiable {
+    var id = UUID()
+    var image: String
+    var content: String
+}
